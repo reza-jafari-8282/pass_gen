10f5e13feaf37e0cbd53406f7b6c1e9843298f66
Reza Jafari <97598188+reza-jafari-8282@users.noreply.github.com>
2022-03-25 19:09:15 +0430

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -2,15 +2,16 @@ import random
 import string
 
 
-length = input("Character length (n <= 94) : ")
+length = input("Character length (n <= 81) : ")
 length = int(length)
 
 lower = string.ascii_lowercase
 upper = string.ascii_uppercase
 digits = string.digits
-symbols = string.punctuation
-
+symbols = '~!@#$%^&*()-_=+./<>'
 all = lower + upper + digits + symbols
+
+
 password = "".join(random.sample(all, length))
 
 print(password)
