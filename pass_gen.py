0731ad94ca82176a08f0660dd8a0766cf133cc45
Reza Jafari <97598188+reza-jafari-8282@users.noreply.github.com>
2022-03-31 15:37:24 +0430

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -1,17 +1,13 @@
 import random
 import string
 
+def passGen(length):
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    digits = string.digits
+    symbols = '~!@#$%^&*()-_=+./<>'
+    all = lower + upper + digits + symbols
+    return "".join(random.sample(all, length))
+length = int(input("Character length (n <= 81) : "))
 
-length = input("Character length (n <= 81) : ")
-length = int(length)
-
-lower = string.ascii_lowercase
-upper = string.ascii_uppercase
-digits = string.digits
-symbols = '~!@#$%^&*()-_=+./<>'
-all = lower + upper + digits + symbols
-
-
-password = "".join(random.sample(all, length))
-
-print(password)
+print(passGen(length))
